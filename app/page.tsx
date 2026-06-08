b6d4a98854aa29281bc4a6f25894e77938e84cdb
theartcode <theartcod01@gmail.com>
2026-06-08 15:17:04 +0530

    fix: import casing for footer in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { PlacementSection } from "@/components/RealJobs";
 import PeopleSection from "@/components/Placed";
 import { PartnersSection } from "@/components/Rectangles";
 import { ChallengeSection } from "@/components/Lakh";
-import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/footer";
 import { StudentWorks } from "@/components/StudentWorks";
 import TutorsSection from "@/components/Tutors";
 
